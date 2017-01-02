45e07ea09e2471877684a24825e69ebeded844cb
arid2875 <amariddell@gmail.com>
2017-01-02 14:15:31 -0800

    Add account activation

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  test "only show activated users on index" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        if user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
+      end      
+    end
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -21,20 +38,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
     assert is_logged_in?
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
-    delete logout_path
-    assert_not is_logged_in?
-    assert_redirected_to root_url
-    delete logout_path
-    follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    unless @user.activated?
+      assert_redirected_to root_url
+    else
+      assert_redirected_to @user
+      follow_redirect!            
+      assert_template 'users/show'
+      assert_select "a[href=?]", login_path, count: 0
+      assert_select "a[href=?]", logout_path
+      assert_select "a[href=?]", user_path(@user)
+      delete logout_path
+      assert_not is_logged_in?
+      assert_redirected_to root_url
+      delete logout_path
+      follow_redirect!
+      assert_select "a[href=?]", login_path
+      assert_select "a[href=?]", logout_path,      count: 0
+      assert_select "a[href=?]", user_path(@user), count: 0
+    end
 
   end
 
